7e927d0de71acb1a874e7fc7f29d9de2e7782e3c
NRekits <64814187+NRekits@users.noreply.github.com>
2021-05-14 13:40:19 -0500

    Avance conexión con MongoDB

--- Server/index.js
+++ Server/index.js
@@ -1,8 +1,28 @@
 const express = require('express');
+const mongoose = require('mongoose')
+const bodyParser= require('body-parser');
 const app = express();
+const port =3000;
 
-app.use(express.static(__dirname + '/public/'));
+mongoose.connect('mongodb://localhost:27017/react', {useNewUrlParser:true, useUnifiedTopology:true});
 
-app.listen('3000', function() {
+const conexion = mongoose.connection;
+conexion.once('open',()=>{
+  console.log('conexion exitosa');
+});
+
+const Alumno= mongoose.model('alumno', {nombre:String});
+//app.use(express.static(__dirname + '/public/'));
+app.use(express.static('/curso',express.static( './index.html')));
+
+app.get('/consultar', (req,res)=>{
+Alumno.find().then(doc=>{
+  res.json({data:doc})
+}).catch(err=>{
+  console.log("error",err.message);
+})
+
+})
+app.listen(port, function() {
   console.log('Servidor web escuchando en el puerto 3000');
 });
