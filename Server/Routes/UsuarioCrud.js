7a0d0f3e64fb49e01bd500ce2b597db34edf106c
NRekits <64814187+NRekits@users.noreply.github.com>
2021-06-01 23:40:18 -0500

    Peticiones de Usuario

--- Server/Routes/UsuarioCrud.js
+++ Server/Routes/UsuarioCrud.js
@@ -4,7 +4,9 @@ const libro = require("../Models/Libro");
 const pedido = require("../Models/Pedido");
 const editorial = require("../Models/Editorial");
 const Usuario = require("../Models/Usuario");
+var mongoose = require('mongoose');
 
+//Añadir usuario
 router.post("/Usuario/Registro", (req, res) => {
   console.log("hola");
   const user = new usuario({
@@ -15,27 +17,10 @@ router.post("/Usuario/Registro", (req, res) => {
   });
 
   const savedUser = user.save();
-  res.json({
-    error: null,
-    data: savedUser,
-  });
+  res.json({ response: "Añadido" });
 });
 
-router.put("/Usuario/InsertarDireccion/:id", (req, res) => {
-  const id = req.params.id;
-  Usuario.findByIdAndUpdate(
-    { _id: id },
-    { $push: { 'Direccion': {
-       Pais: "México",} } }
-  )
-    .then((doc) => {
-      res.json({ response: "ok" });
-    })
-    .catch((err) => {
-      console.log("error al cambiar", err.message);
-    });
-});
-
+//Modificar usuario
 router.put("/Usuario/Modificar/:id", (req, res) => {
   const id = req.params.id;
   const Nom = req.body.Nombre;
@@ -47,20 +32,111 @@ router.put("/Usuario/Modificar/:id", (req, res) => {
     { $set: { Nombre: Nom, Apellido: ape, Contrasena: Contra, Email: Ema } }
   )
     .then((doc) => {
-      res.json({ response: "ok" });
+      res.json({ response: "Usuario Modificado" });
     })
     .catch((err) => {
       console.log("error al cambiar", err.message);
     });
 });
 
+//Eliminar usuario
 router.get("/Usuario/Eliminar/:id", (req, res) => {
-  Usuario.findByIdAndUpdate({ _id: id })
+  const id = req.params.id;
+  Usuario.findByIdAndDelete({ _id: id })
     .then((doc) => {
-      res.json({ response: "ok" });
+      res.json({ response: "Eliminado" });
     })
     .catch((err) => {
       console.log("error al cambiar", err.message);
+    });
+});
+
+//Añadir dirección
+router.put("/Usuario/InsertarDireccion/:id", (req, res) => {
+  const id = req.params.id;
+  const pais = req.body.pais;
+  const estado = req.body.estado;
+  const ciudad = req.body.ciudad;
+  const colonia = req.body.colonia;
+  const calle = req.body.calle;
+  const numero_int = req.body.num;
+  const codigo = req.body.cod;
+
+  Usuario.findByIdAndUpdate(
+    { _id: id },
+    {
+      $push: {
+        Direccion: {
+          Pais: pais,
+          Estado: estado,
+          Ciudad: ciudad,
+          Colonia: colonia,
+          Calle: calle,
+          Numero_int: numero_int,
+          Codigo_postal: codigo,
+        },
+      },
+    }
+  )
+    .then((doc) => {
+      res.json({ response: "Dirección agregada" });
+    })
+    .catch((err) => {
+      console.log("error al cambiar", err.message);
+    });
+});
+
+//Modificar dirección
+router.put("/Usuario/ModificarDireccion/:id_us/:id_dir", (req, res) => {
+
+  const id = req.params.id_us;
+  const id_dir = mongoose.Types.ObjectId(req.params.id_dir);
+  const pais = req.body.pais;
+  const estado = req.body.estado;
+  const ciudad = req.body.ciudad;
+  const colonia = req.body.colonia;
+  const calle = req.body.calle;
+  const numero_int = req.body.num;
+  const codigo = req.body.cod;
+
+  Usuario.updateOne(
+    { _id: id, 'Direccion._id': id_dir },
+    {
+      $set: {
+        'Direccion.$': {
+          Pais: pais,
+          Estado: estado,
+          Ciudad: ciudad,
+          Colonia: colonia,
+          Calle: calle,
+          Numero_int: numero_int,
+          Codigo_postal: codigo,
+        },
+        }
+    }
+     
+  
+  )
+    .then((doc) => {
+      res.json({ response: "Direccion modificada" });
+    })
+    .catch((err) => {
+      console.log("error al cambiar", err );
+    });
+});
+//Eliminar dirección
+router.get("/Usuario/EliminarDireccion/:id_us/:id_dir", (req, res) => {
+  const id = req.params.id_us;
+  const id_dir = req.params.id_dir;
+  Usuario.updateOne(
+    { _id: id },
+    { $pull: { Direccion: { _id: id_dir } } }
+  )
+    .then((doc) => {
+      res.json({ response: "Dirección eliminada" });
+    })
+    .catch((err) => {
+      console.log("error al eliminar", err.message);
     });
 });
 
