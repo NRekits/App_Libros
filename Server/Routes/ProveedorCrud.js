16c28461ef1ebf8483210cd23cab4cc462530394
NRekits <64814187+NRekits@users.noreply.github.com>
2021-06-10 00:30:59 -0500

    Editorial back

--- Server/Routes/ProveedorCrud.js
+++ Server/Routes/ProveedorCrud.js
@@ -6,29 +6,23 @@ var mongoose = require('mongoose');
 // validation
 const Joi = require('@hapi/joi');
 
-//Añadir Libro
-router.post("/Pedido/Insertar", async (req, res) => {
+//Añadir editorial
+router.post("/Editorial/Insertar", async (req, res) => {
   try{
-
-    const book = new libro({
-        Titulo: req.body.titulo,
-        Autor: req.body.autor,
-        Id_editorial:req.body.editorial,
-        Precio: req.body.precio,
-        Cantidad_dis: req.body.cantidad,
-        Fecha_adquision: req.body.fecha,
-        Sinopsis:req.body.sinopsis,
-        Genero: req.body.genero,
-        // pendiente Imagen: { type:String },
-        Formato:req.body.formato,
+    const edit = new editorial({
+        Nombre_encargado: req.body.nomencargado,
+        Ape_encargado: req.body.apeencargado,
+        Nombre_editorial: req.body.nomedit,
+        Email: req.body.email,
+        Tel:req.body.tel,
    
     });
   
-    const savedBook = book.save();
+    const savedEdit = edit.save();
     res.json({
       error: null,
       response: "Añadido",
-      data: savedBook
+      data: savedEdit
   })
    
 
@@ -38,78 +32,51 @@ router.post("/Pedido/Insertar", async (req, res) => {
  
 });
 
-//Ver libro
-router.get("/Libro/Ver/:id", async (req, res) => {
+//Ver editorial
+router.get("/Editorial/Ver/:id",  (req, res) => {
   const id = req.params.id;
-  libro.findById({ _id: id }).then((doc) => {
+  editorial.findById({ _id: id }).then((doc) => {
     res.json({ data: doc, error:null });
   })
    
 });
 
-//Ver libros por generos
-router.get("/Libro/VerGenero/:genero", async (req, res) => {
-    const genero = req.params.genero;
-    libro.find({Genero:genero}).then((doc) => {
+//Ver todos las editoriales
+router.get("/Editorial/VerTodos",  (req, res) => {
+    editorial.find({}).then((doc) => {
         res.json({ data: doc, error:null });
       })
 
   });
 
-//Ver todos los libros
-router.get("/Libro/VerTodos", async (req, res) => {
-    libro.find({}).then((doc) => {
-        res.json({ data: doc, error:null });
-      })
-
-  });
-
-//Ver libros por precio
-router.get("/Libro/Ver/:id", async (req, res) => {
-    const id = req.params.id;
-    libro.find({}).then((doc) => {
-        res.json({ data: doc, error:null });
-      })
-
-  });
-
-//Modificar libro
-router.put("/Libro/Modificar/:id", (req, res) => {
+//Modificar editorial
+router.put("/Editorial/Modificar/:id", (req, res) => {
 
   const id = req.params.id;
-  const titu = req.body.titulo;
-  const au = req.body.autor;
-  const sinop = req.body.sinopsis;
-  const gen = req.body.genero;
-  const edit = req.body.editorial;
-  const pre = req.body.precio;
-  const cant = req.body.cantidad;
-  const vend = req.body.vendidos;
- // pendiente const imag = req.body.imagen;
-  const fecha = req.body.fecha;
-  const format = req.body.formato;
-  
+  const nome = req.body.nomencargado;
+  const apee = req.body.apeencargado;
+  const nom =  req.body.nomedit;
+  const ema = req.body.email;
+  const tel = req.body.tel;
+
   libro.findByIdAndUpdate(
     { _id: id },
-    { $set: { Titulo: titu, Autor: au, Sinopsis:sinop, Genero: gen,
-    Id_editorial: edit, Precio: pre,Cantidad_dis: cant,
-   //pendiente Imagen: { type:String }
-    Vendidos: vend, Fecha_adquision: fecha, Formato:format} }
-  )
+    { $set: {   Nombre_encargado: nome, Ape_encargado:apee,
+        Nombre_editorial: nom, Email: ema, Tel: tel}})
     .then((doc) => {
-      res.json({ response: "Libro Modificado" });
+      res.json({ response: "Editorial Modificado" });
     })
     .catch((err) => {
       console.log("error al cambiar", err.message);
     });
 });
 
-//Eliminar libro
-router.get("/Libro/Eliminar/:id", (req, res) => {
+//Eliminar editorial
+router.get("/Editorial/Eliminar/:id", (req, res) => {
   const id = req.params.id;
-  libro.findByIdAndDelete({ _id: id })
+  editorial.findByIdAndDelete({ _id: id })
     .then((doc) => {
-      res.json({ response: "Libro eliminado" });
+      res.json({ response: "Editorial eliminada" });
     })
     .catch((err) => {
       console.log("error al cambiar", err.message);
